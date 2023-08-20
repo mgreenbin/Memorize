9a0cd4de9b979a244367cf5f3b074e551827099a
Mark Greenberg <magreenberg@gmail.com>
2023-08-20 15:16:36 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mark Greenberg on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
