feb0c9d8997f973c61b807347178532ebd2b7ebf
Mark Greenberg <magreenberg@gmail.com>
2023-08-21 17:10:43 -0500

    Added CardView with emojis to ContentView.swift

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -3,24 +3,54 @@
 //  Memorize
 //
 //  Created by Mark Greenberg on 8/20/23.
+//  for CS193p
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    HStack{
+      CardView(isFaceUp: true)
+      CardView()
+      CardView()
+      CardView()
+      
     }
+    .foregroundColor(.orange)
+    .padding()
+  }
 }
 
+struct CardView: View {
+  var isFaceUp: Bool = false
+  
+  var body: some View {
+    ZStack(content: {
+      if isFaceUp {
+        RoundedRectangle(cornerRadius: 12)
+          .foregroundColor(.white)
+        RoundedRectangle(cornerRadius: 12)
+          .strokeBorder(lineWidth: 2)
+        Text("👻").font(.largeTitle)
+      } else {
+        RoundedRectangle(cornerRadius: 12)
+      }
+       
+    })
+  }
+}
+
+
+
+
+
+
+
+
+// MARK: - Preview code
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
